28323b560bc9d245c7edcb43d2ac57546d1f7b4b
abdksyed <abdksyed@gmail.com>
2021-05-03 11:59:29 +0530

    Indentation

--- mailing/login/mail_crediantials.py
+++ mailing/login/mail_crediantials.py
@@ -39,8 +39,8 @@ def send_mail(sender:str, receiver:str, message:str):
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-        try:
-            server.login(*_crediantials()) #Not storing user input email and password and directly using it to login
+        #try:
+        server.login(*_crediantials()) #Not storing user input email and password and directly using it to login
         # except smtplib.SMTPAuthenticationError:
         #     print('Invalid Credentials. Please Enter Again')
         #     _crediantials = _gmail_crediantials() #Re initializing the gmail_credentials to erase closure vairalbles.
